926c76042cb8c198a008acef61b149c85807a0f9
soadaa <Soadhlh@gmail.com>
2023-10-25 15:26:29 -0400

    Posts to website

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 2, maximum: 80}
+  validates :rating, numericality: { only_integer: true, allow_nil: true, in: 1..10 } 
+end
