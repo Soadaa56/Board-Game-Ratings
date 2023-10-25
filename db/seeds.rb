3b2b15aa45f05a557bbc37faf259928474b552ae
soadaa <Soadhlh@gmail.com>
2023-10-25 16:11:54 -0400

    Page formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Post.create(title: "Sniper Elite: The Board Game", body: "Hidden movement game, 1 vs 3.", rating: "9")
+Post.create(title: "Code Names", body: "Team vs Team word game", rating: "8")
+Post.create(title: "Puerto Rico", body: "One of the original worker placement games", rating: "8")
+Post.create(title: "Sheriff of Nottingham", body: "Free for all game of deception and negotiation.", rating: "7")
+Post.create(title: "Power Grid", body: "Free for all game with bidding and cutthroat area control, similar to ticket to ride", rating: "9")
