2d6f6b8dda9a39525f18ae649aedd971e69e3f95
soadaa <Soadhlh@gmail.com>
2023-10-28 21:41:06 -0400

    Update post from edit and link to show from post title

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,7 @@ class Pin < ApplicationRecord
   has_one_attached :image
   has_many_attached :pictures
   has_rich_text :body
+  belongs_to :post
 
   def image_as_thumbnail
     return unless image.content_type.in?(%w[image/jpeg image/png])
