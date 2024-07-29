ae1f9deb834d3478423b09f4632c9d86e83c3d53
soadaa <Soadhlh@gmail.com>
2024-07-29 17:07:33 -0400

    Average user ratings moved into a partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,10 @@ class Post < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["title", "user", "body", "bgg_ratings"]
   end
+
+  def average_admin_score
+    admin_ratings = ratings.joins(:user).where(users: { admin: true })
+    return nil if admin_ratings.empty?
+    admin_ratings.average(:score)
+  end
 end
