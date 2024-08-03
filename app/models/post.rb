727be8429897d50e50b7533faa6c4af0f61cf05f
soadaa <Soadhlh@gmail.com>
2024-08-03 19:42:23 -0400

    Bgg data fetcher
    Currently just grabs a rating for a board game if you supply the game id. Should be easy to expand if I want to grab any other information from bgg API. I am curious if theres an easier way to get the id for the game itself.
    
    When an admin makes a post for a board game, they will have to supply the game id so it's in the Post table. There might be a way to use the BGG API to search for a board game when making a post that would automatically fill out several columns for the table, most importantly the id number.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum: 2, maximum: 80}
+  validates :bgg_id, presence: true, numericality: { only_integer: true }, allow_nil: true
   belongs_to :user
   has_many :pins
   has_many :ratings
---
+++ lib/services/bgg_data_fetcher.rb
@@ -0,0 +1,24 @@
+require 'faraday'
+require 'nokogiri'
+
+class BggDataFetcher
+  BASE_URL = 'https://api.geekdo.com/xmlapi/boardgame/'.freeze
+
+  def initialize(game_id)
+    @game_id = game_id
+  end
+
+  def fetch_board_game_rating
+    conn = Faraday.get("#{BASE_URL}#{@game_id}?stats=1")
+
+    if conn.status == 200
+      doc = Nokgogiri::XML(conn.body)
+      game_average_rating = doc.at_xpath("//average").to_f.
+
+      game_average_rating ? game_average_rating.round(1) : nil
+    else
+      puts "Connection failed: #{conn.status}"
+      nil
+    end
+  end
+end
