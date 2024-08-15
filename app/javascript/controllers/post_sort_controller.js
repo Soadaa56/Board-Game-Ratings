0088bb85fbdd9519b5815b1ab1ae8759e2f4f4b5
soadaa <Soadhlh@gmail.com>
2024-08-15 12:18:35 -0400

    Post sort controller send route paramter for posts#index

--- app/javascript/controllers/post_sort_controller.js
+++ app/javascript/controllers/post_sort_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="post-sort"
 export default class extends Controller {
+  static targets = ["select"];
   connect() {
-    console.log("Hello stimulus", this.element);
+    console.log("post-sort controller connected");
+  }
+
+  update() {
+    this.sortPosts();
+  }
+
+  sortPosts() {
+    const sortBy = this.selectTarget.value;
+    const url = `/posts?sort_by=${sortBy}`;
+
+    Turbo.visit(url);
   }
 }
