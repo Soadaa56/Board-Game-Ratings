76e4b4ee8edffebfeaf38b0b6a992f3488f0d892
soadaa <Soadhlh@gmail.com>
2024-08-17 12:16:32 -0400

    Remove script for updating database with image urls

--- try.rb
+++
@@ -1,6 +0,0 @@
-Post.all.each do |post|
-  bgg_id = post.bgg_id
-  description = BggDataFetcher.fetch_board_game_details(bgg_id).first[:game_description]
-  post.body = description
-  post.save
-end
