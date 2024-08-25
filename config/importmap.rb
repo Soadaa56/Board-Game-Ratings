fd2eb30719bbca1d557f8fecbd91684b5b407f2b
soadaa <Soadhlh@gmail.com>
2024-08-25 19:55:46 -0400

    Clean up files for esbuild use

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,17 +1,10 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
-
-# causing a bunch of 404 errors in console
-# pin "bootstrap" # @5.3.3
-# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-
-
-# Possibly unneeded, testing with commented out
-# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-# pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
-# pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" # Including bootstrap bundle as a single file including popper
+pin_all_from "app/javascript/controllers", under: "controllers"
