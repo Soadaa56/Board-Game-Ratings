369554caed4ad2491073ebbbbda4c4c81ca008aa
soadaa <Soadhlh@gmail.com>
2024-08-25 20:16:33 -0400

    Fix typo for Stimulus register

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import GameSelectorController from "./controllers/game_selector_controller.js"
 import ModalsController from "./controllers/modals_controller.js"
 import PostSortController from "./controllers/post_sort_controller.js"
 
-window.Stimulus = Application.start
+window.Stimulus = Application.start();
 Stimulus.register("dark-mode", DarkModeController)
 Stimulus.register("game-selector", GameSelectorController)
 Stimulus.register("modals", ModalsController)
