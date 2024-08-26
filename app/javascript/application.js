b2586e5ab09d620b15d0108fcb082425f512e5e8
soadaa <Soadhlh@gmail.com>
2024-08-26 14:25:21 -0400

    Working use of importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-import {DarkModeController} from "controllers/dark_mode_controller"
-import {GameSelectorController} from "controllers/game_selector_controller"
-import {ModalsController} from "controllers/modals_controller"
-import {PostSortController} from "controllers/post_sort_controller"
+import "@hotwired/turbo-rails"
+import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-pin "application", preload: true
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
-pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.1.1/app/assets/javascripts/actioncable.esm.js"
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.1.1/app/assets/javascripts/activestorage.esm.js"
-pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@7.1.1/app/assets/javascripts/actiontext.esm.js"
-pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.7/dist/trix.esm.js"
-pin_all_from "app/javascript/controllers", under: "controllers", to: "controllers"
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
