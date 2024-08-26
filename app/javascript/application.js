53623a4fe8e335ad9e157733b974ada975401dd1
soadaa <Soadhlh@gmail.com>
2024-08-26 12:56:52 -0400

    Commit for branch switch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import {DarkModeController} from "controllers/dark_mode_controller"
+import {GameSelectorController} from "controllers/game_selector_controller"
+import {ModalsController} from "controllers/modals_controller"
+import {PostSortController} from "controllers/post_sort_controller"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
 # Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "@rails/actioncable", to: "actioncable.esm.js"
-pin "@rails/activestorage", to: "activestorage.esm.js"
-pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "trix"
+pin "application", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.1.1/app/assets/javascripts/actioncable.esm.js"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.1.1/app/assets/javascripts/activestorage.esm.js"
+pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@7.1.1/app/assets/javascripts/actiontext.esm.js"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.7/dist/trix.esm.js"
+pin_all_from "app/javascript/controllers", under: "controllers", to: "controllers"
