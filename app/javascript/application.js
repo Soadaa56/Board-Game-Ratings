c72b6a76674865c858f4b8292fcf036503da3dfc
soadaa <Soadhlh@gmail.com>
2024-08-27 13:52:04 -0400

    Import controllers directly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,17 +3,3 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "@popperjs/core"
-// import "jquery"
-
-
-// import { Application } from "@hotwired/stimulus"
-// import DarkModeController from "./controllers/dark_mode_controller.js"
-// import GameSelectorController from "./controllers/game_selector_controller.js"
-// import ModalsController from "./controllers/modals_controller.js"
-// import PostSortController from "./controllers/post_sort_controller.js"
-
-// const application = Application.start();
-// application.register("dark-mode", DarkModeController)
-// application.register("game-selector", GameSelectorController)
-// application.register("modals", ModalsController)
-// application.register("post-sort", PostSortController)
